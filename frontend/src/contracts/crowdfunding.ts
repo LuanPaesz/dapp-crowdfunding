b9b22fd459ece08d9000bc016f531d8dc8958b6d
Luan Bernardes Paes <luanbernardespaes@gmail.com>
2026-03-12 22:47:52 +0000

    Fix contract to vercel

--- frontend/src/contracts/crowdfunding.ts
+++ frontend/src/contracts/crowdfunding.ts
@@ -1,11 +1,2 @@
 // frontend/src/contracts/crowdfunding.ts
-
-// ✅ Importa direto do artifacts do smart-contract (monorepo)
-import CrowdfundingArtifact from "../../../smart-contract/artifacts/contracts/Crowdfunding.sol/Crowdfunding.json";
-
-// Ajuste se seu caminho for diferente.
-// Ex: artifacts/contracts/Crowdfund.sol/Crowdfund.json
-
-export const CROWDFUND_ABI = CrowdfundingArtifact.abi;
-
-export const CROWDFUND_ADDRESS = (import.meta.env.VITE_CROWDFUND_ADDRESS as string) ?? "";
+export { CROWDFUND_ABI, CROWDFUND_ADDRESS } from "../lib/contract";
